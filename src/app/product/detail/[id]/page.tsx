6e9b16bd5b6d3c86a92d591276991ef0890a93f0
WearTime <rizqiwiransyah0410@gmail.com>
2024-06-19 01:07:10 +0700

    Intercepting Routes

---
+++ src/app/product/detail/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { getData } from "@/services/product";
+import Image from "next/image";
+
+export default async function DetailProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const product = await getData(
+    `http://localhost:3000/api/product?id=${params.id}`
+  );
+  return (
+    <div className="ccontainer mx-auto my-10">
+      <div className="w-1/2 mx-auto border border-gray-700">
+        <img
+          src={product.data.image}
+          alt=""
+          className="w-full object-cover aspect-square col-span-2"
+        />
+        <div className="bg-white p-4 px-6">
+          <h3>{product.data.title}</h3>
+          <p>Price: ${product.data.price}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
